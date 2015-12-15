bfac7e8542d2601c70fdd615974526081aca4b7d
Dario-94 <dariogenoese@libero.it>
2015-12-15 12:44:02 +0100

    file modified

--- PHP/analisi_controller.php
+++ PHP/analisi_controller.php
@@ -5,10 +5,30 @@
 	}
 
 //tramite explode conta le parole che ci sono nel testo inserito
-	$testo_arr= explode("", $testo);
+	$testo_arr= explode(" ", $testo);
 	echo "numero parole=" . count($testo_arr);
+	
+	$frequenzaparole=[];
+	foreach($testo_arr as $parola) {
+		if(isset($frequenzaparole[$parola])) {
+			$frequenzaparole[$parola]++;
 
+		} else {
+
+			$frequenzaparole[$parola]=1;
+		}
+
+	}
 	
+
+	arsort($frequenzaparole);	
+
+	foreach($frequenzaparole as $parola => $cont) {
 	
-//il comando action va a richiamare la pagina che abbiamo inserito
+		echo $parola . "=" . $cont . "<br />";
+	
+	}
+//il comando action va a richiamare la pagina che abbiamo inserito.
+//il secondo foreach è un foreach su array associativo.
+//arsort stampa in ordine decrescente.
 ?>
