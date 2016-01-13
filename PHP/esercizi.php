175c33b0cb78157b2b6e638dc3a52ddff535863a
Dario-94 <dariogenoese@libero.it>
2016-01-13 16:34:29 +0100

    file modified

--- PHP/esercizi.php
+++ PHP/esercizi.php
@@ -12,17 +12,17 @@
 //function iva($a, $b) {
 	//$risultato = $a+(($a*$b)/100);
 	//return $risultato;
-}
+//}
 
-function modulo($a, $b) {
-	$risultato = $a % $b;
-	return $risultato;
-}
+//function modulo($a, $b) {
+	//$risultato = $a % $b;
+	//return $risultato;
+//}
 
-	$a = $_GET["price"];
-	$b = $_GET["tax"];	
+	//$a = $_GET["price"];
+	//$b = $_GET["tax"];	
 
-	echo $restodivisione = modulo($a, $b);
+	//echo $restodivisione = modulo($a, $b);
 	 
 	//$a = $_GET["price"];
 	//$b = $_GET["tax"];
@@ -30,4 +30,47 @@ function modulo($a, $b) {
 
 	//echo $prezzo = iva($a, $b)
 	//echo $risultato = $a+(($a*$b)/100);
+
+/*function modulo($numero) {
+	if ($numero < 0) {
+		$numero = -$numero;
+	}
+	return $numero;
+}
+
+	$result = modulo(-5);
+	echo 8 + $result;
+	*/
 ?>
+
+<?php
+
+class Numero {
+
+private $n;
+
+public function __construct($numero) {
+
+		$this->n = $numero;
+	}
+
+public function modulo() {
+
+	if ($this->n < 0) {
+
+			return -$this->n;
+
+		}
+
+		return $this->$n;
+
+	}	
+
+}
+
+/*Istanzio nuovo oggetto di classe Numero*/
+$a = new Numero(-5);
+$b = new Numero(100);
+
+/*eseguo modulo sull'oggetto di tipo numero*/
+echo $a->modulo();
